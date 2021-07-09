d650d6b6d3f5fb5be07f0062476f24a9ecc51c7b
stavroulapier <stavroulapier20@hotmail.com>
2021-07-09 16:50:33 +0300

    Add form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,10 +31,9 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li>
-      <MealItem key={meal.id} meal={meal} />
-    </li>
+    <MealItem key={meal.id} meal={meal} />
   ));
+
   return (
     <section className={classes.meals}>
       <Card>
