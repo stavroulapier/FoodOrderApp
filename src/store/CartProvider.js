8450847afbb61cac81b2b0f382cbb9515e25f8d1
stavroulapier <stavroulapier20@hotmail.com>
2021-07-12 12:39:19 +0300

    Make items removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -12,10 +12,10 @@ const cartReducer = (state, action) => {
     const existingICartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
+    const existingCartItem = state.items[existingICartItemIndex];
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    const existingCartItem = state.items[existingICartItemIndex];
-    
+
     let updatedItems;
     if (existingCartItem) {
       const updatedItem = {
@@ -32,7 +32,29 @@ const cartReducer = (state, action) => {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
-  } else if (action.type === 'REMOVE') return;
+  } else if (action.type === 'REMOVE') {
+    const existingICartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingICartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingICartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
   return defaultCartState;
 };
 
